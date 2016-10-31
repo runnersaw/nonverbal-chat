cc9404b556967ff72b0130cf3dab5384124c4b18
SelinaW <selina@audiocommon.com>
2016-10-30 20:54:15 -0400

    User able to select text colors

--- Space Chat/www/js/touch-recognizer.js
+++ Space Chat/www/js/touch-recognizer.js
@@ -1,8 +1,8 @@
 
 var canvas = document.getElementById("canvas");
 
-document.addEventListener("touchstart", handleEnd, false);
-document.addEventListener("click", handleClick, false);
+canvas.addEventListener("touchstart", handleEnd, false);
+canvas.addEventListener("click", handleClick, false);
 
 function handleClick(evt) {
 	touchEnded(evt.pageX, evt.pageY);
@@ -31,18 +31,56 @@ input.keydown(function (evt) {
     }
 });
 
-function enteredText(input) {
-	drawText(input.val(), input.position().left, input.position().top + input.height(), input.css('font-size'));
+var currentColor = 	'#000000';
 
+function enteredText(input) {
+	drawText(input.val(), input.position().left, input.position().top + input.height(), input.css('font-size'), currentColor);
 	input.hide();
 	input.val('');
 }
 
-function drawText(text, x, y, fontSize) {
+function drawText(text, x, y, fontSize, currentColor) {
 	var c = document.getElementById("canvas");
 	var ctx = c.getContext("2d");
 	ctx.font = fontSize+" sans-serif";
+	ctx.fillStyle = currentColor;
 	ctx.fillText(text,x,y);
+}
+
+var blue_button = $('#blue-button');
+blue_button.click(colorBlue);
+
+function colorBlue() {
+	$(".color-button").css({
+		'border': '1px solid #000000'
+	})
+	var input = $("#blue-button");
+	input.css({
+		'border': '5px solid #0000FF'
+	});
+	var output = $("#input-text");
+	output.css({
+		color: '#0000FF'
+	})
+	currentColor = '#0000FF';
+}
+
+var red_button = $('#red-button');
+red_button.click(colorRed);
+
+function colorRed() {
+	$(".color-button").css({
+		'border': '1px solid #000000'
+	})
+	var input = $("#red-button");
+	input.css({
+		'border': '5px solid #FF0000'
+	});
+	var output = $("#input-text");
+	output.css({
+		color: '#FF0000'
+	})
+	currentColor = '#FF0000';
 }
 
 function log(x) {
