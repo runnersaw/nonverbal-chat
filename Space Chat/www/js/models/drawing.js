1c1fe9e9dae91d76db584b1e65a7986f6954d5b9
SelinaW <selina@audiocommon.com>
2016-12-04 19:35:24 -0500

    I broke something

--- Space Chat/www/js/models/drawing.js
+++ Space Chat/www/js/models/drawing.js
@@ -17,11 +17,25 @@ define(function() {
 		  clickDrag.push(dragging);
 		}
 
-		this.draw = function(canvas) {
-			var ctx = canvas.getContext("2d");
-			ctx.font = this.fontSize+" Verdana";
-			ctx.fillStyle = this.color;
-			ctx.fillText(this.text,this.x,this.y);
+		function redraw(canvas){
+			context = canvas.getContext("2d");
+			context.clearRect(0, 0, canvas.width, canvas.height); // Clears the canvas
+
+			context.strokeStyle = "#df4b26";
+			context.lineJoin = "round";
+			context.lineWidth = 5;
+					
+			for(var i=0; i < clickX.length; i++) {		
+			context.beginPath();
+			if(clickDrag[i] && i){
+			  context.moveTo(clickX[i-1], clickY[i-1]);
+			 }else{
+			   context.moveTo(clickX[i]-1, clickY[i]);
+			 }
+			 context.lineTo(clickX[i], clickY[i]);
+			 context.closePath();
+			 context.stroke();
+			}
 		}
 	};
 
